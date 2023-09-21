b49a71dd5ad0803975beec9e7470fc1002dc9f29
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-09-20 21:27:24 -0400

    docs: 아두이노 시작하기 <피에조 스피커> 정리

---
+++ 아두이노 시작하기/10 피에조 스피커/PiezoelectricSpeaker.cpp
@@ -0,0 +1,17 @@
+// C++ code
+
+void setup()
+{
+  // 도 레 미 파 솔 라 시 도
+  int melody[] = {262, 294, 330, 349, 392, 440, 494, 523};
+  tone(8, melody[0], 500);
+  delay(500);
+  tone(8, melody[1], 500);
+  delay(500);
+  tone(8, melody[2], 500);
+  delay(500);
+}
+
+void loop()
+{
+}
